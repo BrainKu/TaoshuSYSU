4665fec9c5cdf0388f989467ccb403f470fda82b
kuxinwei <kuxinwei@163.com>
2014-06-04 14:16:24 +0800

    change drawer style

--- src/com/sysu/taosysu/MainActivity.java
+++ src/com/sysu/taosysu/MainActivity.java
@@ -1,20 +1,22 @@
 package com.sysu.taosysu;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
 
-import com.sysu.taosysu.ui.fragment.BookListFragment;
-import com.sysu.taosysu.ui.fragment.NavigationDrawerFragment;
+import com.sysu.taosysu.ui.adapter.DrawerAdapter;
 
-public class MainActivity extends ActionBarActivity implements
-		NavigationDrawerFragment.NavigationDrawerCallbacks {
+public class MainActivity extends ActionBarActivity {
 
-	private NavigationDrawerFragment mNavigationDrawerFragment;
+	private ListView mDrawerList;
+	private DrawerLayout mDrawerLayout;
+	private ActionBarDrawerToggle mDrawerToggle;
 
 	private CharSequence mTitle;
 
@@ -23,19 +25,28 @@ public class MainActivity extends ActionBarActivity implements
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		mNavigationDrawerFragment = (NavigationDrawerFragment) getSupportFragmentManager()
-				.findFragmentById(R.id.navigation_drawer);
 		mTitle = getTitle();
 
-		mNavigationDrawerFragment
-				.setUp((DrawerLayout) this.findViewById(R.id.drawer_layout));
-	}
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerList = (ListView) findViewById(R.id.drawer_container);
 
-	@Override
-	public void onNavigationDrawerItemSelected(int position) {
-		FragmentManager fragmentManager = getSupportFragmentManager();
-		fragmentManager.beginTransaction()
-				.replace(R.id.container, new BookListFragment()).commit();
+		mDrawerList.setAdapter(new DrawerAdapter(getApplicationContext()));
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+				R.drawable.ic_drawer, R.string.navigation_drawer_open,
+				R.string.navigation_drawer_close) {
+			@Override
+			public void onDrawerOpened(View drawerView) {
+				// TODO Auto-generated method stub
+				super.onDrawerOpened(drawerView);
+			}
+
+			@Override
+			public void onDrawerClosed(View drawerView) {
+				// TODO Auto-generated method stub
+				super.onDrawerClosed(drawerView);
+			}
+		};
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
 	}
 
 	public void restoreActionBar() {
@@ -47,11 +58,6 @@ public class MainActivity extends ActionBarActivity implements
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		if (!mNavigationDrawerFragment.isDrawerOpen()) {
-			getMenuInflater().inflate(R.menu.main, menu);
-			restoreActionBar();
-			return true;
-		}
 		return super.onCreateOptionsMenu(menu);
 	}
 
